477035f11f0747b2f6266b666fb87c02cc81220e
Abubakar672 <abubakarmujahid1@live.com>
2021-08-11 15:43:39 +0500

    Signin Page Updated

--- FrontEnd/src/components/Screens/Signin.js
+++ FrontEnd/src/components/Screens/Signin.js
@@ -9,7 +9,7 @@ const Signin =()=>{
             <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
            />
            <button class ="btn waves-effect waves-light"> 
-           Verify</button>
+           Next</button>
        </div>
        </div>
     )
