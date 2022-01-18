3e8c49f6c40b441400ebae5ac218109f2ae2458c
Murtaza Ali <61330248+murtazaali1999@users.noreply.github.com>
2022-01-18 14:50:09 +0500

    Delete Keys directory
    keys.js file removed because of MongoDb authentication key for DB access.

--- Keys/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MONGOURI:
-    // "mongodb://127.0.0.1:27017/myFirstDatabase",
-    "mongodb+srv://admin:pliskinsnake@cluster1.aqyvr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  //   "mongodb+srv://admin:pliskinsnake@cluster1.aqyvr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  JWTKEY: "10RKFVAPOVJ913REFZ0BJ3", //for candidate and voter route
-  // ADMINKEY:"@5GJVA0$#0V91N+-03RJF9-A-R9FNAVKD93N" //for admin routes\special routes
-};
